b2968478e474a8d3702a5a857dbaa211ed7d386f
Daniel Gonçalves <danielgg0831@gmail.com>
2020-10-31 18:44:51 -0300

    fix: change the header component of login name

--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Footer from '@/presentation/components/footer/Footer'
-import Header from '@/presentation/components/header/Header'
+import Header from '@/presentation/components/login-header/LoginHeader'
 import Styles from './login-styles.scss'
 import Input from '@/presentation/components/input/Input'
 import FormStatus from '@/presentation/components/form-status/FormStatus'
+import { LoginHeader } from '@/presentation/components'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <Header />
+      <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
         <Input autoComplete="off" type="email" name="email" placeholder="E-mail" />
