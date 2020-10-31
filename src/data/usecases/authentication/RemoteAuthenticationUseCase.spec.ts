9349e125b6d3e0fa5ca428602732446469c0c452
Daniel Gonçalves <danielgg0831@gmail.com>
2020-10-31 00:31:03 -0300

    test(RemoteAuthenticationUseCase): throw UnexpectedError if HttpPostClient returns 400 or 404 or 500

--- src/data/usecases/authentication/RemoteAuthenticationUseCase.spec.ts
+++ src/data/usecases/authentication/RemoteAuthenticationUseCase.spec.ts
@@ -5,6 +5,7 @@ import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/HttpPostCl
 import { mockAuthenticationParams } from '@/domain/test/mock-authentication'
 import { HttpStatusCode } from '@/data/protocols/http/HttpResponse'
 import { InvalidCredentialsError } from '@/domain/errors/InvalidCredentialsError'
+import { UnexpectedError } from '@/domain/errors/UnexpectedError'
 
 interface SutType {
   sut: RemoteAuthenticationUseCase
@@ -28,7 +29,7 @@ const makeSut = (): SutType => {
 describe('RemoteAuthenticationUseCase', () => {
   it('should call HttpPostClient with correct URL and body', async () => {
     const { sut, httpPostClientStub } = makeSut()
-    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: HttpStatusCode.NO_CONTENT }))
+    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: 1 }))
 
     await sut.auth(authenticationParams)
 
@@ -38,9 +39,33 @@ describe('RemoteAuthenticationUseCase', () => {
 
   it('should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
     const { sut, httpPostClientStub } = makeSut()
-    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: HttpStatusCode.UNAUTHORIZED }))
+    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: HttpStatusCode.UNATHORIZED }))
 
     const promise = sut.auth(authenticationParams)
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+
+  it('should throw UnexpectedError if HttPostClient returns 400', async () => {
+    const { sut, httpPostClientStub } = makeSut()
+    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: HttpStatusCode.BAD_REQUEST }))
+
+    const promise = sut.auth(authenticationParams)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttPostClient returns 404', async () => {
+    const { sut, httpPostClientStub } = makeSut()
+    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: HttpStatusCode.NOT_FOUND }))
+
+    const promise = sut.auth(authenticationParams)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttPostClient returns 500', async () => {
+    const { sut, httpPostClientStub } = makeSut()
+    httpPostClientStub.post.mockReturnValueOnce(Promise.resolve({ statusCode: HttpStatusCode.SERVER_ERROR }))
+
+    const promise = sut.auth(authenticationParams)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
