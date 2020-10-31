d7228c8464b06a22c7896851eb817af7368c7b36
Daniel Gonçalves <danielgg0831@gmail.com>
2020-10-31 18:54:12 -0300

    feat: add app router

--- src/presentation/components/login-header/LoginHeader.tsx
+++ src/presentation/components/login-header/LoginHeader.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import Styles from './header-styles.scss'
+import Styles from './login-header-styles.scss'
 import Logo from '@/presentation/components/logo/Logo'
 
 const LoginHeader: React.FC = () => {
