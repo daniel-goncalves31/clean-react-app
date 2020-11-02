9faea11bb6c9b5fbbd5795bdc8cfae055b39d47d
Daniel Gonçalves <danielgg0831@gmail.com>
2020-11-02 11:32:21 -0300

    test(ValidationBuilder): return a MinLengthFieldValidation on min

--- src/validation/validators/ValidationBuider.spec.ts
+++ src/validation/validators/ValidationBuider.spec.ts
@@ -1,5 +1,6 @@
 import { random } from 'faker'
 import { EmailValidation } from './EmailValidation'
+import { MinLengthValidation } from './MinLengthValidation'
 import { RequiredFieldValidation } from './RequiredFieldValidation'
 import { ValidationBuilder } from './ValidationBuilder'
 
@@ -8,7 +9,6 @@ interface SutType {
 }
 
 const field = random.word()
-
 const makeSut = (): SutType => {
   const sut = ValidationBuilder.field(field)
 
@@ -18,15 +18,22 @@ const makeSut = (): SutType => {
 }
 
 describe('ValidationBuilder', () => {
-  it('should return RequiredFieldValidator', async () => {
+  it('should return RequiredFieldValidation', async () => {
     const { sut } = makeSut()
     const validations = sut.required().build()
     expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
-  it('should return EmailValidator', async () => {
+  it('should return EmailValidation', async () => {
     const { sut } = makeSut()
     const validations = sut.email().build()
     expect(validations).toEqual([new EmailValidation(field)])
+  })
+
+  it('should return MinLengthValidation', async () => {
+    const { sut } = makeSut()
+    const minLength = 1
+    const validations = sut.min(minLength).build()
+    expect(validations).toEqual([new MinLengthValidation(field, minLength)])
   })
 })
