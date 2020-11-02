5d9c4788f7ea0a8573aed146c893d5b949db3d67
Daniel Gonçalves <danielgg0831@gmail.com>
2020-11-01 21:43:08 -0300

    test(RequiredFieldValidation): return null if field is not empty

--- src/validation/required-field/RequiredFieldValidation.spec.ts
+++ src/validation/required-field/RequiredFieldValidation.spec.ts
@@ -1,9 +1,12 @@
+import { random } from 'faker'
 import { RequiredFieldError } from '../errors'
 import { RequiredFieldValidation } from './RequiredFieldValidation'
 
 interface SutType {
   sut: RequiredFieldValidation
 }
+
+const validValue = random.words(4)
 
 const makeSut = (): SutType => {
   const sut = new RequiredFieldValidation('email')
@@ -18,5 +21,11 @@ describe('RequiredFieldValidation', () => {
     const { sut } = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return null if field is not empty', async () => {
+    const { sut } = makeSut()
+    const error = sut.validate(validValue)
+    expect(error).toBeFalsy()
   })
 })
