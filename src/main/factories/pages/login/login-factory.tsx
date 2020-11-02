aeb83413c54f3c91fc141bb598f05dade574976f
Daniel Gonçalves <danielgg0831@gmail.com>
2020-11-02 11:56:05 -0300

    refactor: move login related factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,22 +1,13 @@
-import { RemoteAuthenticationUseCase } from '@/data/usecases/authentication/RemoteAuthenticationUseCase'
-import { AxiosHttpClient } from '@/infra/http/axios-http-client/AxiosHttpClient'
 import { Login } from '@/presentation/pages'
-import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
 import React from 'react'
+import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authenticaion-factory'
+import { makeLoginValidation } from './login-validation-factory'
 
 interface Props { }
 
 const makeLogin: React.FC<Props> = () => {
-  const url = 'any_url'
-  const axiosHttpClient = new AxiosHttpClient()
-  const remoteAuthentication = new RemoteAuthenticationUseCase(url, axiosHttpClient)
-  const validationComposite = ValidationComposite.bind([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
-
   return (
-    <Login validation={validationComposite} authentication={remoteAuthentication} />
+    <Login validation={makeLoginValidation()} authentication={makeRemoteAuthentication()} />
   )
 }
 
---
+++ src/main/factories/usecases/authentication/remote-authenticaion-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteAuthenticationUseCase } from '@/data/usecases/authentication/RemoteAuthenticationUseCase'
+import { AuthenticationUseCase } from '@/domain/usecases/AuthenticationUseCase'
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
+import { makeAxiosHttpClient } from '../../http/axios-http-client-factory'
+
+export const makeRemoteAuthentication = (): AuthenticationUseCase => {
+  return new RemoteAuthenticationUseCase(makeApiUrl(), makeAxiosHttpClient())
+}
