c29b2a28b80366997e749f51ef81694c6820a0c4
Daniel Gonçalves <danielgg0831@gmail.com>
2020-11-01 21:53:45 -0300

    feat(EmailValidation): return null if email is valid

--- src/validation/validators/EmailValidation.ts
+++ src/validation/validators/EmailValidation.ts
@@ -4,6 +4,10 @@ import { FieldValidation } from '../protocols/FieldValidation'
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) { }
   validate(email: string): Error {
-    return new InvalidFieldError()
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    if (!emailRegex.test(email)) {
+      return new InvalidFieldError()
+    }
+    return null
   }
 }
