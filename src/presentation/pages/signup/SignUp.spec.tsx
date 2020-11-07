712cb30aa8a69b3ce93153bd338db7e1f40a8def
Daniel Gonçalves <danielgg0831@gmail.com>
2020-11-07 10:53:46 -0300

    test(SignUp): show valid state if Validation succeeds

--- src/presentation/pages/signup/SignUp.spec.tsx
+++ src/presentation/pages/signup/SignUp.spec.tsx
@@ -14,7 +14,7 @@ type SutTypes = {
 const validationError = 'Required Field'
 const value = random.word()
 
-const makeSut = (): SutTypes => {
+const makeSut = (validationError: string = ''): SutTypes => {
   const validationStub = mock<Validation>()
   validationStub.validate.mockReturnValue(validationError)
 
@@ -29,7 +29,7 @@ const makeSut = (): SutTypes => {
 
 describe('SignUp Page', () => {
   it('should start with initial state', () => {
-    const { sut } = makeSut()
+    const { sut } = makeSut(validationError)
     TestFormHelper.testChildCount(sut, 'error-wrap', 0)
     TestFormHelper.testButtonIsDisabled(sut, 'submit', true)
     TestFormHelper.testStatusForField(sut, 'name', validationError)
@@ -40,27 +40,51 @@ describe('SignUp Page', () => {
 
   describe('Validation', () => {
     it('should show name error if Validation fails', async () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut(validationError)
       TestFormHelper.populateField(sut, 'name', value)
       TestFormHelper.testStatusForField(sut, 'name', validationError)
     })
 
     it('should show email error if Validation fails', async () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut(validationError)
       TestFormHelper.populateField(sut, 'email', value)
       TestFormHelper.testStatusForField(sut, 'email', validationError)
     })
 
     it('should show password error if Validation fails', async () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut(validationError)
       TestFormHelper.populateField(sut, 'password', value)
       TestFormHelper.testStatusForField(sut, 'password', validationError)
     })
 
     it('should show passwordConfirmation error if Validation fails', async () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut(validationError)
       TestFormHelper.populateField(sut, 'passwordConfirmation', value)
       TestFormHelper.testStatusForField(sut, 'passwordConfirmation', validationError)
+    })
+
+    it('Should show valid name state if Validation succeeds', () => {
+      const { sut } = makeSut()
+      TestFormHelper.populateField(sut, 'name', value)
+      TestFormHelper.testStatusForField(sut, 'name')
+    })
+
+    it('Should show valid email state if Validation succeeds', () => {
+      const { sut } = makeSut()
+      TestFormHelper.populateField(sut, 'email', value)
+      TestFormHelper.testStatusForField(sut, 'email')
+    })
+
+    it('Should show valid password state if Validation succeeds', () => {
+      const { sut } = makeSut()
+      TestFormHelper.populateField(sut, 'password', value)
+      TestFormHelper.testStatusForField(sut, 'password')
+    })
+
+    it('Should show valid passwordConfirmation state if Validation succeeds', () => {
+      const { sut } = makeSut()
+      TestFormHelper.populateField(sut, 'passwordConfirmation', value)
+      TestFormHelper.testStatusForField(sut, 'passwordConfirmation')
     })
   })
 })
