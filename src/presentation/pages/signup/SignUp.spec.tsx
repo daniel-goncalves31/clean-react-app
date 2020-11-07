f5dc061b602bdd171c6c9e08fd4d481ac16abd9d
Daniel Gonçalves <danielgg0831@gmail.com>
2020-11-07 00:38:49 -0300

    test(SignUp): show name error if Validation fails

--- src/presentation/pages/signup/SignUp.spec.tsx
+++ src/presentation/pages/signup/SignUp.spec.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import SignUp from './SignUp'
-import { RenderResult, render } from '@testing-library/react'
+import { RenderResult, render, fireEvent } from '@testing-library/react'
 import * as TestFormHelper from '@/presentation/test/FormHelper'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { Validation } from '@/presentation/protocols/Validation'
+import { random } from 'faker'
 
 type SutTypes = {
   sut: RenderResult
+  validationStub: MockProxy<Validation>
 }
 
+const validationError = 'Required Field'
+const value = random.word()
+
 const makeSut = (): SutTypes => {
+  const validationStub = mock<Validation>()
+  validationStub.validate.mockReturnValue(validationError)
+
   const sut = render(
-    <SignUp />
+    <SignUp validation={validationStub} />
   )
   return {
-    sut
+    sut,
+    validationStub
   }
 }
 
+const populateField = (sut: RenderResult, filedName: string, value: string): void => {
+  const input = sut.getByTestId(filedName) as HTMLInputElement
+  fireEvent.input(input, { target: { value } })
+}
+
 describe('SignUp Page', () => {
-  test('should start with initial state', () => {
-    const validationError = 'Required Field'
+  it('should start with initial state', () => {
     const { sut } = makeSut()
     TestFormHelper.testChildCount(sut, 'error-wrap', 0)
     TestFormHelper.testButtonIsDisabled(sut, 'submit', true)
@@ -26,5 +41,13 @@ describe('SignUp Page', () => {
     TestFormHelper.testStatusForField(sut, 'email', validationError)
     TestFormHelper.testStatusForField(sut, 'password', validationError)
     TestFormHelper.testStatusForField(sut, 'passwordConfirmation', validationError)
+  })
+
+  describe('Validation', () => {
+    it('should show name error if Validation fails', async () => {
+      const { sut } = makeSut()
+      populateField(sut, 'name', value)
+      TestFormHelper.testStatusForField(sut, 'name', validationError)
+    })
   })
 })
